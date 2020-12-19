e145c21c2208acd372fddc5dcf4d02f6010855e4
DagothMor <siegfried1997@gmail.com>
2020-12-19 17:39:58 +0300

    Insecure random example

---
+++ SecretSolution/ErrorClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Web.Security;
+
+
+/// Используя Random.Next для генерации нескольких случайных чисел для пароля, метод возвращает 1 и то же число при постоянных его вызовах,на самом деле каждый экземпляр вызванный пустым контруктором, при одинаковом начальным состоянием(текущая дата и время) будет по своей сути одинаков по отношению к другим.
+string GeneratePassword()
+{
+
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(GenerateDigit());
+    }
+    ...
+static int GeneratePassword()
+    {
+        Random rng = new Random();
+        // Предположим, что здесь много логики
+        return rng.Next(10);
+    }
+}
---
+++ SecretSolution/WrongWay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	/// <summary>
+	/// теперь генерятся разные числа но если вызвать несколько раз то сами наборы в нескольких
+	/// экземплярах будут равны ибо проблема кроется в том же одинаковом значении при инициализа-
+	/// ции.Сам класс Random не потокобезопасен.
+	/// </summary>
+	public Class1()
+	{
+		Random rng = new Random();
+		for (int i = 0; i < 100; i++)
+		{
+			Console.WriteLine(GenerateDigit(rng));
+		}
+	}
+	static int GenerateDigit(Random rng)
+	{
+		return rng.Next(10);
+	}
+}
